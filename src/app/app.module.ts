45a7ff4dd81492d523214a1a99775e89eda02e03
bestnacufzxzx <bestnacufzxzx1@gmail.com>
2019-05-08 16:52:38 +0700

    barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ZBar } from '@ionic-native/zbar/ngx';
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,  FormsModule],
   providers: [
     StatusBar,
     SplashScreen,
+    ZBar,
+    BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { ZBar, ZBarOptions } from '@ionic-native/zbar/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+    zbarOptions:any;
+  scannedResult:any;
+ 
+  constructor(
+    private zbar: ZBar
+  ) {
+ 
+    this.zbarOptions = {
+      flash: 'off',
+      drawSight: false
+    }
+ 
+  }
+ 
+  scanCode(){
+    this.zbar.scan(this.zbarOptions)
+   .then(result => {
+      console.log(result); // Scanned code
+      this.scannedResult = result;
+   })
+   .catch(error => {
+      alert(error); // Error message
+   });
+  }
+ 
+}
